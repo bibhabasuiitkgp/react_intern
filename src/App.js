0d4e55bc5ae9afd47110183a1057107777c4461a
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2024-05-15 21:25:25 +0530

    Footer and Navbar done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import Hero from "./Pages/Hero_Section/Hero";
 import Cards from "./Pages/Cards_intro/Cards";
 import "./App.css";
 import Timeline from "./Pages/Timeline/Timeline";
+import Footer from "./Pages/Footer/Slider";
+import Navbar from "./Pages/Navbar/Navbar";
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Hero />
       <Cards />
       <Timeline />
+      <Cards />
+      <Footer />
     </div>
   );
 }
---
+++ src/Pages/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.css'; // Importing the CSS file for styling
+
+function Navbar() {
+    return (
+        <nav>
+            <h4>mahmoud anwar</h4>
+            <ul>
+                <li><a href="header9.html">home</a></li>
+                <li><a href="#">about us <i className="fas fa-chevron-down"></i></a></li>
+
+                <li><a href="header9.html">projects</a></li>
+                <li><a href="header9.html">news</a></li>
+                <li><a href="header9.html">gallery</a></li>
+                <li><a href="header9.html">blog</a></li>
+                <li><a href="header9.html">contact</a></li>
+
+
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
